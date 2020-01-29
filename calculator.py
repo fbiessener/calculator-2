dcfa8ea4a955869f2344a46b5057eb75a1c09f02
Your Name <you@example.com>
2020-01-29 12:52:39 -0800

    Calculator-2 completed

--- calculator.py
+++ calculator.py
@@ -17,60 +17,59 @@ from arithmetic import *
 
 
 def calculator():
-
-    user_input = input("")
-
-    tokens = user_input.split(" ")
-
     count = 0
 
-    # if 2 or fewer tokens
-    if len(tokens) <= 2:
-        return "Not enough inputs"
+    while True:
 
-    while count < len(tokens):
+        user_input = input("")
+        tokens = user_input.split(" ")
+
         # quits
         if tokens[0] == "q":
             break
 
+        # if 2 or fewer tokens
+        if len(tokens) <= 2:
+            print("Not enough inputs")
+
         # This solves addition inputs
         elif tokens[0] == "+":
-            return (add(float(tokens[1]), float(tokens[2])))
+            print(add(float(tokens[1]), float(tokens[2])))
 
         # This solves subtraction inputs
         elif tokens[0] == "-":
-            return (subtract(float(tokens[1]), float(tokens[2])))
+            print(subtract(float(tokens[1]), float(tokens[2])))
 
 
         # This solves multiply inputs
         elif tokens[0] == "*":
-            return (multiply(float(tokens[1]), float(tokens[2])))
+            print(multiply(float(tokens[1]), float(tokens[2])))
 
 
         # This solves divide inputs
         elif tokens[0] == "/":
-            return (divide(float(tokens[1]), float(tokens[2])))
+            print(divide(float(tokens[1]), float(tokens[2])))
 
 
         # This solves square inputs
         elif tokens[0] == "square":
-            return (square(float(tokens[1])))
+            print(square(float(tokens[1])))
 
 
         # This solves cube inputs
         elif tokens[0] == "cube":
-            return (cube(float(tokens[1])))
+            print(cube(float(tokens[1])))
 
         # This solves pow inputs
         elif tokens[0] == "pow":
-            return (pow(float(tokens[1]), float(tokens[2])))
+            print(pow(float(tokens[1]), float(tokens[2])))
 
         # This solves mod inputs
         elif tokens[0] == "mod":
-            return (mod(float(tokens[1]), float(tokens[2])))
+            print(mod(float(tokens[1]), float(tokens[2])))
 
         else:
-            return "Please enter an operator followed by two integers"
+            print("Please enter an operator followed by two integers")
 
 
         count += 1
