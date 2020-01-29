907c5b66bba8cb65f8cb8ca0d75b7046c78a3aca
Your Name <you@example.com>
2020-01-29 12:28:52 -0800

    Successfully calling arithmetic, not using user input correctly.

--- calculator.py
+++ calculator.py
@@ -16,17 +16,65 @@ from arithmetic import *
 #        decide which math function to call based on first token
 
 
-def calculator(input_str):
-    tokens = input_str.split(" ")
+def calculator():
 
-    if tokens[0] == "+":
-        return (add(int(tokens[1]), int(tokens[2]))
+    user_input = input("")
+
+    tokens = user_input.split(" ")
+
+    count = 0
+
+    # if 2 or fewer tokens
+    if len(tokens) <= 2:
+        return "Not enough inputs"
+
+    while count < len(tokens):
+        # quits
+        if tokens[0] == "q":
+            break
+
+        # This solves addition inputs
+        elif tokens[0] == "+":
+            return (add(float(tokens[1]), float(tokens[2])))
+
+        # This solves subtraction inputs
+        elif tokens[0] == "-":
+            return (subtract(float(tokens[1]), float(tokens[2])))
 
 
-#calculator("+ 2 3")
+        # This solves multiply inputs
+        elif tokens[0] == "*":
+            return (multiply(float(tokens[1]), float(tokens[2])))
 
 
-# "Not enough inputs"
-# "Please enter an operator followed by two integers"
+        # This solves divide inputs
+        elif tokens[0] == "/":
+            return (divide(float(tokens[1]), float(tokens[2])))
 
-# Your code goes here
+
+        # This solves square inputs
+        elif tokens[0] == "square":
+            return (square(float(tokens[1])))
+
+
+        # This solves cube inputs
+        elif tokens[0] == "cube":
+            return (cube(float(tokens[1])))
+
+        # This solves pow inputs
+        elif tokens[0] == "pow":
+            return (pow(float(tokens[1]), float(tokens[2])))
+
+        # This solves mod inputs
+        elif tokens[0] == "mod":
+            return (mod(float(tokens[1]), float(tokens[2])))
+
+        else:
+            return "Please enter an operator followed by two integers"
+
+
+        count += 1
+
+
+calculator()
+
